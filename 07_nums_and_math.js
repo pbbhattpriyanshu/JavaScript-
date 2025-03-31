b7deff1ea4eb29756aba3fc2a4dc597bea013755
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-03-31 21:59:02 +0530

    math and nums

---
+++ 07_nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 400;
+//console.log(score);
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+
+let otherNumber = 123.599;
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundread = 1000000;
+//console.log(hundread.toLocaleString("en-IN")); // 1,000,000
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++]
+
+// Math
+
+// console.log(Math);
+// console.log(Math.PI); // 3.141592653589793
+// console.log(Math.abs(-10)); // 10
+// console.log(Math.abs(16)); // 16
+// console.log(Math.sqrt(16)); // 4
+// console.log(Math.pow(3, 3)); // 27
+// console.log(Math.round(3.7)); // 4
+// console.log(Math.random());
+
+function dice() {
+    console.log(Math.floor(Math.random() * 6 )+ 1); 
+}
+
+dice()
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(otherNumber.toPrecision(3));
