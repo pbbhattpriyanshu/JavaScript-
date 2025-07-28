dc61dc2fddfb81a693a2ebe8c763b26c3b940242
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-07-28 21:07:44 +0530

    Conversion - DONE!

--- 03_conversion.js
+++ 03_conversion.js
@@ -4,14 +4,18 @@ console.log("LECTURE 3 - CONVERSION");
 // let score = null;
 // let score = undefined;
 // let score = true;
-let score = 1
+// let score = 1
 
 
-console.log(score);
-console.log(typeof score);  // null --> object // 89v ---> string/number
+// console.log(score);
+// console.log(typeof score);  // null --> object // 89v ---> string/number
 
-let valInNumber = String(score); //Number = string convert number
-console.log(typeof valInNumber);
-console.log(valInNumber); // NaN --> Not a number
+// let valInNumber = String(score); //Number = string convert number
+// console.log(typeof valInNumber);
+// console.log(valInNumber); // NaN --> Not a number
+
+console.log(1 + "2");//12
+console.log("Piyush" * 2);//NaN
+
 
 
