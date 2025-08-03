3ab74c7db8c991313a012ec3bf76b549fa37c2d5
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-08-03 18:27:54 +0530

    function

---
+++ 10_function.js
@@ -0,0 +1,61 @@
+// functions
+
+//Syntax
+function functionName(parameters) {
+  // Code to run (machine's work)
+  return someValue; // optional
+}
+
+// Example 1: Function Without Input
+function sayHello() {
+  console.log("Hello there!");
+}
+
+// Call (use the machine)
+sayHello();
+
+// Example 2: Function With Input (Parameters)
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Example 2.1: Function With or without Input (default Parameters)
+function sub(val1 = 0, val2 = 0) {
+    console.log(val1 - val2);
+}
+
+// Example 2.2: Function With multiple Input ( Rest Operator (...))
+//Explanation:
+// ...numbers collects all arguments into an array [1,2,3,4,5].
+// Perfect for unknown number of inputs.
+
+function sumOfAll(...numbers) {
+    return numbers.reduce((sum, num) => sum + num, 0);
+}
+
+console.log(sumOfAll(1,2,3,4,5));
+
+// Example 2.3  Spread Operator (...)
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+let combined = [...arr1, ...arr2];
+
+console.log(combined); // [1, 2, 3, 4, 5, 6]
+
+
+
+// Example 3: Function That Returns Something
+function add(a, b) {
+  return a + b;
+}
+
+//function expression
+let func = function () {
+    console.log("lolo");
+}
+
+// Arrow function
+let wishes = () => {
+    console.log("Happy birthday");
+}
