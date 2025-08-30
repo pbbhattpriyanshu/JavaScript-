3ac466766627062f436e67d0e93d6a29c49b14bc
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-08-30 22:34:40 +0530

    Key-value Structure, Dot vs bracket notation

--- 12_Objects.js
+++ 12_Objects.js
@@ -1,5 +1,6 @@
 // Key-value Structure
 let obj = {
+    //key : Value
     name: "Piyush",
     age: 21,
     role: "AI Engineer",
@@ -8,7 +9,13 @@ let obj = {
 // Dot vs bracket notation
 
 //values access
-console.log(obj.age);
-console.log(obj['isOnline']);
+console.log(obj.age); //dot
+console.log(obj['isOnline']); //bracket
+
+// Why we use bracket notation for accessing values
+let aa = 'name';
+console.log(obj.aa); //undefined
+
+console.log(obj[aa]); //piyush
 
 
