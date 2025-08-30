9a95d6c2f1b48f5afa7cafb90a2921e3da79501a
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-08-30 22:28:34 +0530

    Intro to object

---
+++ 12_Objects.js
@@ -0,0 +1,14 @@
+// Key-value Structure
+let obj = {
+    name: "Piyush",
+    age: 21,
+    role: "AI Engineer",
+    isOnline: true,
+}
+// Dot vs bracket notation
+
+//values access
+console.log(obj.age);
+console.log(obj['isOnline']);
+
+
